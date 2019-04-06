4d91d00f339e346678e12081b9c55746dc3cb165
Karim Djemai <karimdjemai1997@gmail.com>
2019-04-06 12:58:02 +0200

    add comment how the getRoute route should work

--- lib/index.php
+++ lib/index.php
@@ -8,11 +8,16 @@
 	
 	$app->get('/route', function (Request $request, Response $response, array $args) {
 		//HVV
+			//get route
+			//all all stations out of route
 		
 		//Stadtrad
-		
+			//for all stations out of the route check:
+				//whats the nearest Stadtrad station
+				// how many bikes are there
+		  
 		//algorithmus
-		
+			//whats the suggestion 
 		
 		//return
 		return $response->getBody()->write(json_encode([
